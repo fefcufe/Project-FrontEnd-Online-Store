c9d7b7a275a376d3adf99804b4e02ea4d548638c
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-03-10 16:37:46 -0300

    cumprindo o requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 export async function getCategories() {
-  // Implemente aqui
+  const urlCategories = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const resultsCategories = await urlCategories.json();
+  return resultsCategories;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! ando o izer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(category, query) {
+  const urlCategoriesQuery = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${category}&${query}`);
+  const resultsCategoriesQuery = await urlCategoriesQuery.json();
+  return resultsCategoriesQuery;
 }

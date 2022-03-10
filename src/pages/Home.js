27b8b9840da8878cce4c9a81ea1d6d2887980654
Fernanda Ferreira <fefcufe@gmail.com>
2022-03-10 18:48:43 -0300

    Requisito 3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
     return (
-      <h3 data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </h3>
+      <div>
+        <h3 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h3>
+        <button type="button">
+          <Link
+            to="/shoppingCart"
+            data-testid="shopping-cart-button"
+          >
+            Carrinho de compras
+            {' '}
+
+          </Link>
+        </button>
+      </div>
+
     );
   }
 }

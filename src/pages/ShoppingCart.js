15c329e75357df73f1adc8d577b7c5ee214b224d
Igormcf <igormaciel82@gmail.com>
2022-03-14 18:53:29 -0300

    Ends requirement 10

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -18,6 +18,47 @@ class ShoppingCart extends React.Component {
     });
   }
 
+  handleQuantitySub = (id) => {
+    const { shoppingCart } = this.state;
+    const productSearched = shoppingCart.findIndex((object) => object.id === id);
+    const productAux = shoppingCart[productSearched];
+    shoppingCart[productSearched] = {
+      ...productAux,
+      quantity: productAux.quantity - 1,
+    };
+    if (shoppingCart[productSearched].quantity === 0) {
+      shoppingCart.splice(productSearched, 1);
+    }
+    this.setState({
+      shoppingCart: [...shoppingCart],
+    });
+    localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
+  }
+
+  handleQuantitySum = (id) => {
+    const { shoppingCart } = this.state;
+    const productSearched = shoppingCart.findIndex((object) => object.id === id);
+    const productAux = shoppingCart[productSearched];
+    shoppingCart[productSearched] = {
+      ...productAux,
+      quantity: productAux.quantity + 1,
+    };
+    this.setState({
+      shoppingCart: [...shoppingCart],
+    });
+    localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
+  }
+
+  handleBtnRemove = (id) => {
+    const { shoppingCart } = this.state;
+    const productSearched = shoppingCart.findIndex((object) => object.id === id);
+    shoppingCart.splice(productSearched, 1);
+    this.setState({
+      shoppingCart: [...shoppingCart],
+    });
+    localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
+  }
+
   render() {
     const { shoppingCart } = this.state;
     return shoppingCart.length === 0 ? (
@@ -40,15 +81,38 @@ class ShoppingCart extends React.Component {
                 </strong>
               </p>
               <img src={ thumbnail } alt="imagem produto" />
-              <p>{ price * quantity }</p>
               <p data-testid="shopping-cart-product-quantity">
                 quantity:
                 {' '}
                 {quantity}
               </p>
+              <button
+                data-testid="product-decrease-quantity"
+                type="button"
+                onClick={ () => this.handleQuantitySub(id) }
+                value={ quantity }
+              >
+                -
+              </button>
+              <button
+                data-testid="product-increase-quantity"
+                type="button"
+                onClick={ () => this.handleQuantitySum(id) }
+                value={ quantity }
+              >
+                +
+              </button>
+              <button
+                type="button"
+                onClick={ () => this.handleBtnRemove(id) }
+              >
+                X
+              </button>
+              <p>{ price * quantity }</p>
             </li>
           ))}
         </ul>
+        <button type="button">Finalizar Compra</button>
       </div>
     );
   }
